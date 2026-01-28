ab5f185a095fb01e94b648e1b5f18ae3b58a22c3
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2026-01-28 13:17:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Your JS code here.
+const sliderImages = document.querySelectorAll('.slide-in');
+ function checkSlide() {
+    sliderImages.forEach((img) => {
+      // Halfway through the image
+      const slideInAt =
+        window.scrollY + window.innerHeight - img.height / 2;
+
+      // Bottom of the image
+      const imageBottom = img.offsetTop + img.height;
+
+      const isHalfShown = slideInAt > img.offsetTop;
+      const isNotScrolledPast = window.scrollY < imageBottom;
+
+      if (isHalfShown && isNotScrolledPast) {
+        img.classList.add('active');
+      } else {
+        img.classList.remove('active');
+		  }
+    });
+  }
+
+  // Attach debounced scroll handler
+  window.addEventListener('scroll', debounce(checkSlide));
